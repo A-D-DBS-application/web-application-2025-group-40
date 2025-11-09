99663a114d25cf3de342ece9358fa8816e0b95ef
lucavanongeval <luca.vanongeval@ugent.be>
2025-11-09 21:33:24 +0100

    Update models.py
    ook JobListing

--- apppp/models.py
+++ apppp/models.py
@@ -1,8 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import ARRAY, DATERANGE
 db = SQLAlchemy()
 
-class User(db.Model):
+class App_user(db.Model):
     __tablename__ = 'app_user'  
 
     id = db.Column(db.Integer, primary_key=True)
@@ -11,6 +12,22 @@ class User(db.Model):
     role = db.Column(db.String(80), unique=False, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)  
 
-
     def __repr__(self):
         return f'<User {self.username}>'
+        
+class JobListing(db.Model):
+    __tablename__ = 'job_listing'  # exact zoals in Supabase
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    employer_id = db.Column(db.BigInteger, nullable=True)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    location = db.Column(db.String(255), nullable=True)
+    salary = db.Column(db.Numeric, nullable=True)
+    periode = db.Column(DATERANGE, nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+    requirements = db.Column(ARRAY(db.Text), nullable=True)
+    auto_accept = db.Column(db.Boolean, nullable=True)
+
+    def __repr__(self):
+        return f'<JobListing {self.title}>'
