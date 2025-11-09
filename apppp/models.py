dc4cb9d4088fc1d7655c3f367de314b018d96ce3
lucavanongeval <luca.vanongeval@ugent.be>
2025-11-09 21:25:36 +0100

    Update models.py
    toevoeging van app_user

--- apppp/models.py
+++ apppp/models.py
@@ -1,2 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'app_user'  
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    role = db.Column(db.String(80), unique=False, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)  
+
+
+    def __repr__(self):
+        return f'<User {self.username}>'
