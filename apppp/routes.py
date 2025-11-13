473482f940517afccf40137bf4658b6cfd87a861
lucavanongeval <luca.vanongeval@ugent.be>
2025-11-13 15:31:47 +0100

    toevoeging matchh page

--- apppp/routes.py
+++ apppp/routes.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+from models import db, Match, AppUser, JobListing
+from datetime import datetime
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
 
 @app.route('/choose_role')
 def choose_role():
@@ -13,6 +19,16 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+@app.route('/matches')
+def match_list():
+    matches = (
+        db.session.query(Match, AppUser, JobListing)
+        .join(AppUser, Match.user_id == AppUser.id)
+        .join(JobListing, Match.job_id == JobListing.id)
+        .all()
+    )
+    return render_template('matches.html', matches=matches)
 
 if __name__ == "__main__":
     app.run(debug=True)
